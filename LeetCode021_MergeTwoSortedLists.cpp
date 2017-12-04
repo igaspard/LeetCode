b45a5da35a037c113d9b6b387b5c15802778eee4
gaspardshen <syuyang@gmail.com>
2017-12-04 21:42:38 +0800

    Add new solution for 021

--- LeetCode021_MergeTwoSortedLists.cpp
+++ LeetCode021_MergeTwoSortedLists.cpp
@@ -6,6 +6,51 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ // w/o use new node
+ class NewSolution {
+ public:
+     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+         ListNode *head = NULL, *prev = NULL;
+
+         if(l1==NULL)
+             return l2;
+         if(l2==NULL)
+             return l1;
+
+         if(l1->val > l2->val) {
+             head = l2;
+             prev = head;
+             l2 = l2->next;
+         }
+         else {
+             head = l1;
+             prev = head;
+             l1 = l1->next;
+         }
+
+         while(l1 && l2) {
+             if(l1->val > l2->val) {
+                 prev->next = l2;
+                 prev = l2;
+                 l2 = l2->next;
+             }
+             else {
+                 prev->next = l1;
+                 prev = l1;
+                 l1 = l1->next;
+             }
+         }
+
+         if(l1==NULL)
+             prev->next = l2;
+         if(l2==NULL)
+             prev->next = l1;
+
+         return head;
+     }
+ };
+
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
