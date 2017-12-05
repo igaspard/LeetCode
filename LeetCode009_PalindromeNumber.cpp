ed6262432e8edeea1f930f5cebf14d73168d66d0
gaspardshen <syuyang@gmail.com>
2017-12-06 02:07:41 +0800

    Add 009 Palindrome Number

---
+++ LeetCode009_PalindromeNumber.cpp
@@ -0,0 +1,40 @@
+class BestSolution {
+public:
+    int isPalindrome(int x) {
+        // negative is not Palindrome, and last digit is zero but x is not zero case
+        if (x < 0 || (x%10 == 0 && x != 0))
+            return false;
+
+        int rev = 0;
+        while(x > rev) { // revert only reach to the middle
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        // handle odd case, get rid of the middle digit
+        return (x == rev || x == rev/10);
+    }
+};
+
+class Solution {
+public:
+    int reverse(int x) {
+        long ans = 0;
+        while(x) {
+            ans = ans*10 + x%10;
+            if (ans > INT_MAX || ans < INT_MIN)
+                return 0;
+            x /= 10;
+        }
+        return (int)ans;
+    }
+    bool isPalindrome(int x) {
+        if (x<0)
+            return false;
+        else {
+            if (x == reverse(x))
+                return true;
+            else
+                return false;
+        }
+    }
+};
