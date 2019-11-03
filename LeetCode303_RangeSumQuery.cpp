1bdb59e1cb9c8e3bf1fb0038e29ef4f3aca95e47
Gaspard Shen <syuyang@gmail.com>
2019-11-03 14:50:46 +0800

    Add 303

---
+++ LeetCode303_RangeSumQuery.cpp
@@ -0,0 +1,33 @@
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        const int n = nums.size();
+        if (n > 0) {
+            sum = new int[n];
+        
+            sum[0] = nums[0];
+            for(int i = 1; i < n; i++) {
+                sum[i] = sum[i-1] + nums[i];
+            }            
+        }        
+    }
+    
+    int sumRange(int i, int j) {
+        if (i > 0)
+            return sum[j] - sum[i-1];
+        else
+            return sum[j];
+    }
+    
+private:
+    int *sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
+
+// Runtime: 28 ms, faster than 93.36% of C++ online submissions for Range Sum Query - Immutable.
+// Memory Usage: 17.2 MB, less than 96.55% of C++ online submissions for Range Sum Query - Immutable.
