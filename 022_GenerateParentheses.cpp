5a7f9888eae778c6231007c74e690b529ae6295c
gaspard.shen <gaspard.shen@wisecore.com.tw>
2020-10-17 17:22:46 +0800

    Permutations problem

--- 022_GenerateParentheses.cpp
+++ 022_GenerateParentheses.cpp
@@ -1,23 +1,63 @@
+class ParenthesesString {
+   public:
+    string str;
+    int openCnt = 0;
+    int closeCnt = 0;
+
+    ParenthesesString(const string& s, int open, int close) {
+        this->str = s;
+        this->openCnt = open;
+        this->closeCnt = close;
+    }
+};
+
 class Solution {
-public:
+   public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        queue<ParenthesesString> queue;
+        queue.push({"", 0, 0});
+
+        while (!queue.empty()) {
+            auto it = queue.front();
+            queue.pop();
+
+            if (it.openCnt == n && it.closeCnt == n) {
+                ans.emplace_back(it.str);
+            } else {
+                if (it.openCnt < n) queue.push({it.str + '(', it.openCnt + 1, it.closeCnt});
+
+                if (it.openCnt > it.closeCnt) queue.push({it.str + ')', it.openCnt, it.closeCnt + 1});
+            }
+        }
+
+        return ans;
+    }
+};
+
+// BFS
+Runtime: 4 ms, faster than 86.42% of C++ online submissions for Generate Parentheses.
+Memory Usage: 8.3 MB, less than 5.14% of C++ online submissions for Generate Parentheses.
+
+class Solution {
+   public:
     void addParentheses(int n, int left, int right, string str, vector<string>& ans) {
         if (right > left)
             return;
         else if (left > n)
             return;
-        else if(str.size() == n*2)
+        else if (str.size() == n * 2)
             ans.emplace_back(str);
 
-        addParentheses(n, left+1, right,   str+"(", ans);
-        addParentheses(n, left  , right+1, str+")", ans);
+        addParentheses(n, left + 1, right, str + "(", ans);
+        addParentheses(n, left, right + 1, str + ")", ans);
     }
 
-    
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;  
-        
-        addParentheses(n, 0, 0, "", ans);  
- 
+        vector<string> ans;
+
+        addParentheses(n, 0, 0, "", ans);
+
         return ans;
     }
 };
