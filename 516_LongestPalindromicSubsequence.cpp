78f4d7676e2646951629090b8b1339b1e26e9032
gaspard.shen <gaspard.shen@wisecore.com.tw>
2021-06-22 10:43:47 +0800

    Add LPS compression

--- 516_LongestPalindromicSubsequence.cpp
+++ 516_LongestPalindromicSubsequence.cpp
@@ -3,13 +3,42 @@ class Solution {
     int longestPalindromeSubseq(string s) {
         const int N = s.length();
 
+        int dp[N];
+        for (int i = 0; i < N; ++i) dp[i] = 1;
+
+        for (int i = N - 2; i >= 0; --i) {
+            int pre = 0;  // save the dp[i+1][j-1]
+            for (int j = i + 1; j < N; ++j) {
+                int temp = dp[j];
+                if (s[i] == s[j])
+                    dp[j] = pre + 2;
+                else
+                    dp[j] = max(dp[j - 1], dp[j]);
+                pre = temp;
+            }
+        }
+
+        return dp[N - 1];
+    }
+};
+
+// dp, 2D compression to 1D
+// time complexity: O(N)
+// Runtime: 32 ms, faster than 99.06% of C++ online submissions for Longest Palindromic Subsequence.
+// Memory Usage: 10.7 MB, less than 83.98% of C++ online submissions for Longest Palindromic Subsequence.
+
+class Solution {
+   public:
+    int longestPalindromeSubseq(string s) {
+        const int N = s.length();
+
         int dp[N][N];
         for (int i = 0; i < N; ++i) {  // base cases
             dp[i][i] = 1;
             if (i != 0) dp[i][i - 1] = 0;
         }
 
-        for (int i = N - 1; i >= 0; --i) {
+        for (int i = N - 2; i >= 0; --i) {
             for (int j = i + 1; j < N; ++j) {
                 if (s[i] == s[j])
                     dp[i][j] = dp[i + 1][j - 1] + 2;
@@ -23,5 +52,6 @@ class Solution {
 };
 
 // dp, 2D, define dp[i][j] as s[i..j] LPS(Longest Palindrome Subseq)
+// time complexity: O(N^2)
 // Runtime: 32 ms, faster than 99.06% of C++ online submissions for Longest Palindromic Subsequence.
 // Memory Usage: 10.7 MB, less than 83.98% of C++ online submissions for Longest Palindromic Subsequence.
