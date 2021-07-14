884092a17f43bb898fe82a76109738260c710726
gaspard.shen <gaspard.shen@wisecore.com.tw>
2021-07-15 01:47:18 +0800

    update

--- 1928_MinimumCosttoReachDestinationinTime.cpp
+++ 1928_MinimumCosttoReachDestinationinTime.cpp
@@ -29,15 +29,15 @@ class Solution {
                 int adj_t = adj.second;
                 int adj_c = passingFees[v];
 
-                if (t + adj_t <= maxTime) {
-                    if (cost[v] > c + adj_c) {
-                        cost[v] = c + adj_c;
-                        time[v] = t + adj_t;
-                        pq.push({cost[v], time[v], v});
-                    } else if (time[v] > t + adj_t) {
-                        time[v] = t + adj_t;
-                        pq.push({c + adj_c, time[v], v});
-                    }
+                if (t + adj_t > maxTime) continue;
+                
+                if (cost[v] > c + adj_c) {
+                    cost[v] = c + adj_c;
+                    time[v] = t + adj_t;                       
+                    pq.push({cost[v], time[v], v});
+                } else if (time[v] > t + adj_t) {
+                    time[v] = t + adj_t;
+                    pq.push({c + adj_c, time[v], v});
                 }
             }
         }
@@ -52,54 +52,3 @@ class Solution {
 // Runtime: 348 ms, faster than 70.31% of C++ online submissions for Minimum Cost to Reach Destination in
 // Time. Memory Usage: 45.1 MB, less than 45.82% of C++ online submissions for Minimum Cost to Reach
 // Destination in Time.
-
-class Solution {
-   public:
-    int minCost(int maxTime, vector<vector<int>> &edges, vector<int> &passingFees) {
-        const int N = passingFees.size();
-
-        vector<bool> visited(N, false);
-        vector<vector<pair<int, int>>> adj(N);
-        for (auto edge : edges) {
-            adj[edge[0]].push_back({edge[1], edge[2]});
-            adj[edge[1]].push_back({edge[0], edge[2]});
-        }
-
-        vector<int> path(N);
-        int idx = 0, time = 0;
-        vector<int> ans;
-        dfs(0, N - 1, adj, visited, path, idx, passingFees, ans, time, maxTime);
-
-        return ans.empty() ? -1 : *min_element(ans.begin(), ans.end());
-    }
-
-   private:
-    void dfs(int s, int d, vector<vector<pair<int, int>>> &adj, vector<bool> &visited, vector<int> &path,
-             int &idx, vector<int> &passingFees, vector<int> &ans, int time, int maxTime) {
-        visited[s] = true;
-        path[idx] = s;
-        ++idx;
-
-        if (s == d) {
-            // for (auto p : path) cout << p << " ";
-            if (time <= maxTime) {
-                int fees = 0;
-                for (int i = 0; i < idx; ++i) {
-                    // cout << path[i] << " ";
-                    fees += passingFees[path[i]];
-                }
-                ans.emplace_back(fees);
-            }
-        } else {
-            for (auto v : adj[s]) {
-                if (!visited[v.first])
-                    dfs(v.first, d, adj, visited, path, idx, passingFees, ans, time + v.second, maxTime);
-            }
-        }
-
-        --idx;
-        visited[s] = false;
-    }
-};
-
-// TLE
