0f21366d54a099ed61341997fa33580936f61709
Gaspard Shen <yshen@roku.com>
2021-10-07 01:51:50 +0800

    Sliding Window

--- 076_MinimumWindowSubstring.cpp
+++ 076_MinimumWindowSubstring.cpp
@@ -1,4 +1,41 @@
 class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> hash, window;
+        for(auto ch : t) ++hash[ch];
+        
+        int left = 0, right = 0, match = 0, min_len = INT_MAX;
+        string ans = "";
+        while (right < s.length()) {
+            char c = s[right++];
+            if (hash.count(c) != 0) {
+                ++window[c];
+                if (hash[c] == window[c]) ++match;
+            }
+            
+            while (match == hash.size()) {
+                if (right - left < min_len) {
+                    min_len = right - left;
+                    ans = s.substr(left, min_len);
+                }
+                
+                char d = s[left++];
+                if (hash.count(d) != 0) {
+                    if (window[d] == hash[d]) --match;
+                    --window[d];
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// sliding window + hash
+// Runtime: 20 ms, faster than 55.41% of C++ online submissions for Minimum Window Substring.
+// Memory Usage: 16.5 MB, less than 14.18% of C++ online submissions for Minimum Window Substring.
+
+class Solution {
    public:
     string minWindow(string s, string t) {
         unordered_map<char, int> hash;
