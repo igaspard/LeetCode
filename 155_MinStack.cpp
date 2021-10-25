62221716c4c5eb366b746bbca3cc344b606539fe
Gaspard Shen <yshen@roku.com>
2021-10-26 00:10:57 +0800

    Oct challenge

--- 155_MinStack.cpp
+++ 155_MinStack.cpp
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode id=155 lang=cpp
- *
- * [155] Min Stack
- */
-
-// @lc code=start
 class MinStack {
 public:
     /** initialize your data structure here. */
@@ -34,13 +27,7 @@ private:
     stack<int> s, min_s;
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
-// @lc code=end
+// push, pop, getMin O(N)
+// Runtime: 27 ms, faster than 45.07% of C++ online submissions for Min Stack.
+// Memory Usage: 16.3 MB, less than 68.88% of C++ online submissions for Min Stack.
 
--- 716_MaxStack.cpp
+++ 716_MaxStack.cpp
@@ -1,3 +1,33 @@
+/*
+Problem
+
+Design a max stack that supports push, pop, top, peekMax and popMax.
+
+push(x) -- Push element x onto stack.
+pop() -- Remove the element on top of the stack and return it.
+top() -- Get the element on the top.
+peekMax() -- Retrieve the maximum element in the stack.
+popMax() -- Retrieve the maximum element in the stack, and remove it. 
+If you find more than one maximum elements, only remove the top-most one.
+
+Example 1:
+
+MaxStack stack = new MaxStack();
+stack.push(5); 
+stack.push(1);
+stack.push(5);
+stack.top(); -> 5
+stack.popMax(); -> 5
+stack.top(); -> 1
+stack.peekMax(); -> 5
+stack.pop(); -> 1
+stack.top(); -> 5
+
+Note: 
+-1e7 <= x <= 1e7 
+Number of operations won't exceed 10000. 
+The last four operations won't be called when stack is empty.
+*/
 class MaxStack {
 public:
     /** initialize your data structure here. */
@@ -31,16 +61,16 @@ public:
     int popMax() {
         int ans = max_s.top();
         max_s.pop();
-        
+        // find the max in the original stack, save into temp stack
         stack<int> t;
         while (s.top() != ans) {
             t.push(s.top());
             s.pop();
         }
-        
+        // find the max one, pop it out, restore the temp back to stack
         s.pop();
         while (!t.empty()) {
-            push(t.top()); // 
+            push(t.top());
             t.pop();
         }
         return ans;
